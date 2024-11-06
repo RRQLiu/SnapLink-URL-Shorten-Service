566db83389c8ae77c08a3b020068f6edf7333631
RRQLiu <rl115@rice.edu>
2024-11-06 12:42:03 -0600

    initial setup

---
+++ snaplink/src/main/java/com/snaplink/SnapLinkApplication.java
@@ -0,0 +1,11 @@
+package com.snaplink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapLinkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SnapLinkApplication.class, args);
+    }
+}
